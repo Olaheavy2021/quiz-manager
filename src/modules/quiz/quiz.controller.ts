4f3b460fde21fef4d165a07457063fc241d5b41c
Sunkanmi Olawuwo <sunkanmiolawuwo.gas@gmail.com>
2022-05-03 18:39:55 +0100

    Validation and DTO

--- src/modules/quiz/quiz.controller.ts
+++ src/modules/quiz/quiz.controller.ts
@@ -1,9 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { CreateQuizDto } from './dto/create-quiz.dto';
+import { QuizService } from './quiz.service';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 
 @Controller('quiz')
 export class QuizController {
+  constructor(private quizService: QuizService) {}
   @Get('/')
   getAllQuiz() {
-    return [1, 2, 3];
+    return this.quizService.getAllQuiz();
+  }
+
+  @Post()
+  @HttpCode(200)
+  @UsePipes(ValidationPipe)
+  createQuiz(@Body() quizData: CreateQuizDto) {
+    return { data: quizData };
   }
 }
--- src/modules/quiz/quiz.module.ts
+++ src/modules/quiz/quiz.module.ts
@@ -1,7 +1,9 @@
+import { QuizService } from './quiz.service';
 import { QuizController } from './quiz.controller';
 import { Module } from '@nestjs/common';
 
 @Module({
   controllers: [QuizController],
+  providers: [QuizService],
 })
 export class QuizModule {}
